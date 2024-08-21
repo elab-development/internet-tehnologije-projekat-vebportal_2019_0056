be04f8b40cc7f4cf14acc7bb866feebc5e5fd2e1
Nadja1212 <nkrstic00@gmail.com>
2024-08-21 22:13:10 +0200

    App.jsx

--- webportal-projekat-react/client/src/App.jsx
+++ webportal-projekat-react/client/src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from '../pages/Home';
-import About from '../pages/About';
-import SignUp from '../pages/SignUp';
-import SignIn from '../pages/SignIn';
-import Dashboard from '../pages/Dashboard';
-import Projects from '../pages/Projects';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
 
 function App() {
   return (
