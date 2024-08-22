4064c2670358a895f59351b8472695a0ecb9a00b
lolalazovic <lazoviclola@gmail.com>
2024-08-22 18:51:05 +0200

    MongoDB Connection

--- webportal-projekat-react/server/index.js
+++ webportal-projekat-react/server/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import connectToMongoDB from './utils/dbConnect.js';
 
 dotenv.config();
 const port = process.env.PORT || 8000;
+
+connectToMongoDB();
 
 const app = express();
 
