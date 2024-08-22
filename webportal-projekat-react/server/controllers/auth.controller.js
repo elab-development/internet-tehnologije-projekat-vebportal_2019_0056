d7c6171c44b98a549202dc5a905c992c0066cdc4
Nadja1212 <nkrstic00@gmail.com>
2024-08-22 22:07:54 +0200

    Error Handler and Error Middleware

--- webportal-projekat-react/server/controllers/auth.controller.js
+++ webportal-projekat-react/server/controllers/auth.controller.js
@@ -1,13 +1,12 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/User.model.js';
+import errorHandler from '../middlewares/errorHandler.js';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (!username || !email || !password) {
-    return res.status(400).json({
-      message: 'Username, Email & Password are required!',
-    });
+    next(errorHandler(400, 'All fields are required'));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
--- webportal-projekat-react/server/index.js
+++ webportal-projekat-react/server/index.js
@@ -24,3 +24,13 @@ app.use('/api/users', userRouter);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
