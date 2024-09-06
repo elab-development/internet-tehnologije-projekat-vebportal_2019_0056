4989662c465a1747a8612fe928d27f0faaafa280
Nadja1212 <nkrstic00@gmail.com>
2024-09-06 14:08:52 +0200

    controller-routes-for-post

--- Laravel-webportal/routes/api.php
+++ Laravel-webportal/routes/api.php
@@ -3,6 +3,8 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\LoginRegisterController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,8 +23,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/categories/{id}', [CategoryController::class, 'show']);
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
 
 Route::post('/register', [LoginRegisterController::class, 'register']);
 Route::post('/login', [LoginRegisterController::class, 'login']);
@@ -34,6 +42,9 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     });
 
     Route::resource('/categories', CategoryController::class)
+        ->only(['store', 'update', 'destroy']);
+
+    Route::resource('/posts', PostController::class)
         ->only(['store', 'update', 'destroy']);
 
 
